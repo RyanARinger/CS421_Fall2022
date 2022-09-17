5bcffba585790e812b75f224a5a27d51601763a1
RyanARinger <RyanARinger1@gmail.com>
2022-09-16 20:50:31 -0700

    generator update

--- HW1B_RecGen/generator.cpp
+++ HW1B_RecGen/generator.cpp
@@ -1,39 +1,21 @@
-#include<iostream>
-#include<string>
+#include <iostream>
+#include <queue>
+#include <string>
+
 using namespace std;
 
-#include<queue>
- // include queue.h or equivalent such as <queue>
+bool recognizer(string s) {
+    int size = s.size();
 
-//----------------------------------------------
-// CS421 HW1 
-// Write a generator in C++ for L = {x | x is any combination of a's and b's}.
-// Your name: Ryan Ringer
-//----------------------------------------------
-
-// Copy the recognizer function here from the other file.
-bool recognizer(string s)
-{
-	int size = s.size();
-
-        for(int i = 0; i < size; i++)
-        {
-        	if(s[i] != 'a' && s[i] != 'b') return false;
-        }
-        return true;
+    for (int i = 0; i < size; i++)
+    {
+        if (s[i] != 'a' && s[i] != 'b') return false;
+    }
+    return true;
 }
 
-
-// main: It should create each string over E = {a,b,c} systematically
-//   (short to long) and pass each string to the recognizer function
-//   created in Part a).  Hint: use a queue to generate strings
-//   Only those strings for which the recognizer returned TRUE
-//   should be displayed. 
-//   Keeps on going until the queue overflows but the user can 
-//   terminate the program with control-C 
-//   after about 20 strings have been displayed.
 int main()
-{ **
+{
     queue<string> language;
     queue<string> toBeAppended;
     string alphabet = "abc";
@@ -48,28 +30,30 @@ int main()
         if (recognizer(temp)) {
             language.push(temp);
             cout << temp << endl;
+            itCounter++;
         }
     }
     
     if(language.size() > 0)
         while (true) {
-            cout << "Continue with one more character? (y/n):";
+            /*cout << "Continue with one more character? (y/n):";
             cin >> choice;
             if (choice != "y" && choice != "yes") {
                 return 1;
-            }
-            /*if (itCounter >= 20) {
-                cout << "Continue for 20 more, or until next character (whichever comes last)? (y/n):";
-                cin >> choice;
-                if (choice != "y" && choice != "yes") {
-                   return 1;
-                }
             }*/
             queueSize = language.size();
             while (!language.empty()) {
                 temp = language.front();
                 language.pop();
                 for (int i = 0; i < alphabet.size(); i++) {
+                    if (itCounter >= 20) {
+                        cout << "Continue for 20 more, or until next character (whichever comes last)? (y/n):";
+                        cin >> choice;
+                        if (choice != "y" && choice != "yes") {
+                            return 1;
+                        }
+                        itCounter = 0;
+                    }
                     nestedTemp = temp + alphabet[i];
                     if (recognizer(nestedTemp)) {
                         toBeAppended.push(nestedTemp);
@@ -91,3 +75,4 @@ int main()
 
     return 0;
 }
+
